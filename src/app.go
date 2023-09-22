0d26789269f19429d76bcb4b0da19f57ad91c1f5
Sakar Ghimire <sakarghimire@SPMBP121.local>
2023-09-22 16:52:26 +0545

    created root page

--- src/app.go
+++ src/app.go
@@ -1,13 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	// Create a new Gin router
+	router := gin.Default()
+
+	// Define a route that renders an HTML page
+	router.GET("/", func(c *gin.Context) {
+		// Use the HTML function to render an HTML response
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Simple Page",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	// Run the web server on port 8080
+	router.Run(":8080")
 }
